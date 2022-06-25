abdb2be7647113618455e6737176d542b2344de6
sawqa172 <pugegy@gmail.com>
2022-06-25 17:45:13 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, ".prettierrc"), "utf8"),
+);
+
+module.exports = {
+  extends: ["prettier", "react-app"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": ["error", prettierOptions],
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
---
+++ src/components/icons/Cross/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+import Icon from "../Icon";
+
+const Svg = styled(Icon)`
+  width: 24px;
+  height: 24px;
+`;
+
+export { Svg };
